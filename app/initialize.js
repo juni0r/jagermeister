f9c41d79f713ad6a8d2d1bb955436c6a35228410
Andreas Korth <andreas.korth@neofonie.de>
2018-11-22 10:37:32 +0100

    Create bottle blend

--- app/initialize.js
+++ app/initialize.js
@@ -1,28 +1,44 @@
-// import TimelineMax from 'gsap';
-// import TweenMax from 'gsap';
-
-// import TweenMax, { Power4 } from 'gsap/src/uncompressed/TweenMax';
-// import EasePack from 'gsap/src/uncompressed/easing/EasePack';
-// import TimelineMax from 'gsap/src/uncompressed/TimelineMax';
-
-import TweenMax, { TimelineLite, Power3, Expo } from 'gsap';
+import $ from 'jquery';
+import TweenMax, { TimelineLite, Power1, Linear } from 'gsap';
+import ScrollMagic from 'scrollmagic';
+import 'scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js';
+import 'scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const bottle = $('#hero-bottle');
-  // const bottle = $('#bottle');
+  // const heroBottle = $('#hero-bottle');
+  const bottle = $('#bottle');
   const frost1 = $('#frost-1');
   const frost2 = $('#frost-2');
 
+  // heroBottle.css({ top: -400 });
   bottle.css({ opacity: 0 });
   frost1.css({ opacity: 0 });
   frost2.css({ opacity: 0 });
 
-  console.log('Initialized app', TimelineLite, TweenMax);
+  const controller = new ScrollMagic.Controller();
+  const timeline = new TimelineLite({ paused: false });
 
-  const timeline = new TimelineLite();
-  const freeze = new TimelineLite();
+  // console.log('Initialized app', TimelineLite, TweenMax, ScrollMagic);
 
-  freeze.to(frost2, 4, { opacity: 1, ease: Expo.easeOut }, '+=0.5');
-  timeline.to(bottle, 2, { opacity: 1, ease: Power3.easeOut }, '+=1');
-  timeline.add(freeze);
+  TweenMax
+    .to(bottle, 1, { opacity: 1, ease: Power1.easeOut, delay: 0.25 })
+    .eventCallback('onComplete', () => {
+      timeline.to(frost1, 3, { opacity: 1, ease: Linear.easeInOut }, '+=2');
+      timeline.to(frost2, 4, { opacity: 1, ease: Linear.easeInOut }, '-=1');
+      timeline.to(bottle, 2, { opacity: 1 });
+
+      frost2.click(() => {
+        timeline.reversed() ? timeline.play() : timeline.reverse();
+      });
+    });
+
+  new ScrollMagic.Scene({
+    triggerElement: '#freeze-start',
+    triggerHook: 'onLeave',
+    duration: 4000
+  })
+    .setPin('#hero-stage')
+    .setTween(timeline)
+    // .addIndicators({ name: '1 duration: 1600' })
+    .addTo(controller);
 });
