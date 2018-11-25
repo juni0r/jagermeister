ff48a4afab103bdf351eb18f21650a4608bce06c
Andreas Korth <andreas.korth@neofonie.de>
2018-11-25 18:07:30 +0100

    Now with smoke

--- app/initialize.js
+++ app/initialize.js
@@ -1,44 +1,102 @@
 import $ from 'jquery';
-import TweenMax, { TimelineLite, Power1, Linear } from 'gsap';
+import TweenMax, { TimelineMax as Timeline, Linear, Expo } from 'gsap';
 import ScrollMagic from 'scrollmagic';
 import 'scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js';
 import 'scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js';
+import { Ease, LayerGenerator, Range } from 'src/smoke';
 
-document.addEventListener('DOMContentLoaded', () => {
-  // const heroBottle = $('#hero-bottle');
-  const bottle = $('#bottle');
+window.$ = $;
+window.Timeline = Timeline;
+window.Tween = TweenMax;
+window.Expo = Expo;
+
+$(() => {
+  const smoke = setupSmoke();
+
   const frost1 = $('#frost-1');
   const frost2 = $('#frost-2');
 
-  // heroBottle.css({ top: -400 });
-  bottle.css({ opacity: 0 });
+  smoke.css({ opacity: 0 });
   frost1.css({ opacity: 0 });
   frost2.css({ opacity: 0 });
 
-  const controller = new ScrollMagic.Controller();
-  const timeline = new TimelineLite({ paused: false });
+  const freeze = new Timeline({ paused: false });
 
-  // console.log('Initialized app', TimelineLite, TweenMax, ScrollMagic);
+  freeze.to(smoke, 0, { opacity: 1 });
+  freeze.to(frost2, 1.5, { opacity: 0.67, ease: Linear.easeOut });
+  freeze.staggerFrom($('#smoke .layer'), 1, { x: 20, y: '-=50', opacity: 0, ease: Linear.easeOut }, 0.025, '-=1.5');
 
-  TweenMax
-    .to(bottle, 1, { opacity: 1, ease: Power1.easeOut, delay: 0.25 })
-    .eventCallback('onComplete', () => {
-      timeline.to(frost1, 3, { opacity: 1, ease: Linear.easeInOut }, '+=2');
-      timeline.to(frost2, 4, { opacity: 1, ease: Linear.easeInOut }, '-=1');
-      timeline.to(bottle, 2, { opacity: 1 });
+  const chill = new Timeline({ paused: false });
 
-      frost2.click(() => {
-        timeline.reversed() ? timeline.play() : timeline.reverse();
-      });
-    });
+  chill.to(frost1, 1, { opacity: 1, ease: Linear.easeInOut });
 
-  new ScrollMagic.Scene({
-    triggerElement: '#freeze-start',
-    triggerHook: 'onLeave',
-    duration: 4000
-  })
-    .setPin('#hero-stage')
-    .setTween(timeline)
-    // .addIndicators({ name: '1 duration: 1600' })
-    .addTo(controller);
+  var duration = 800;
+
+  addScene('#ease-start', {
+    pin: '#hero-stage',
+    duration,
+    tween: chill
+  });
+
+  addScene('#freeze-keep', {
+    pin: '#hero-stage',
+    duration,
+    offset: duration
+  });
+
+  addScene('#freeze-start', {
+    offset: duration,
+    tween: freeze
+  });
 });
+
+const debugScrollMagic = false;
+const controller = new ScrollMagic.Controller();
+
+function addScene (triggerElement, { duration = 0, offset = 0, triggerHook = 'onLeave', tween, pin }) {
+  const scene = new ScrollMagic.Scene({ triggerElement, triggerHook, duration, offset });
+  if (pin) scene.setPin(pin);
+  if (tween) scene.setTween(tween);
+  if (debugScrollMagic) scene.addIndicators({ name: triggerElement });
+  scene.addTo(controller);
+  return scene;
+}
+
+function setupSmoke () {
+  const smoke = $('#smoke').smoke();
+
+  const scale = Ease(2).in;
+
+  const layerCount = 25;
+
+  const xFrom = 80;
+  const xTo = 120;
+  const xJitter = 0;
+
+  const rpsFrom = 30;
+  const rpsTo = -20;
+
+  let generator;
+
+  generator = new LayerGenerator({
+    x: Range({ from: xFrom, to: xTo, jitter: xJitter, scale }),
+    y: Range({ from: 0, to: 700, jitter: 20, scale }),
+    opacity: Range({ from: 1, to: 0, scale }),
+    scale: Range({ from: 0.15, to: 0.5, scale }),
+    rps: Range({ from: rpsFrom, to: rpsTo, scale }),
+    zIndex: Range({ from: 999, to: 1999, scale }),
+    rotation: () => (Math.random() * 360)
+  });
+
+  const layers = [];
+
+  generator.make(layerCount, layers);
+
+  generator.ranges.x = Range({ from: -xFrom, to: -xTo, jitter: xJitter, scale });
+  generator.ranges.rps = Range({ from: -rpsFrom, to: -rpsTo, scale });
+  generator.make(layerCount, layers);
+
+  smoke.smoke('load', layers.sort((a, b) => a.zIndex - b.zIndex));
+
+  return smoke;
+}
---
+++ app/src/smoke.js
@@ -0,0 +1,153 @@
+import $ from 'jquery';
+import TweenMax from 'gsap';
+
+export default class Smoke {
+  constructor (el, props) {
+    Object.assign(this, props);
+    this.el = el;
+    this.layers = [];
+    this.lastAnimationFrameAt = 0;
+    this.animate = this.animate.bind(this);
+    window.requestAnimationFrame(this.animate);
+  }
+
+  addLayer (props) {
+    const layer = new Layer(props);
+    this.layers.push(layer);
+    layer.appendTo(this.el);
+    return layer;
+  }
+
+  removeLayerAt (index) {
+    const layer = this.layerAt(index);
+    this.layers = this.layers.slice(0, index).concat(this.layers.slice(index + 1));
+    layer.remove();
+    return layer;
+  }
+
+  layerAt (index) {
+    return this.layers[index];
+  }
+
+  removeAllLayers () {
+    this.layers.forEach(layer => layer.remove());
+    this.layers = [];
+  }
+
+  load (layers) {
+    layers.forEach(layer => this.addLayer(layer));
+  }
+
+  animate (timestamp) {
+    const timeElapsed = timestamp - this.lastAnimationFrameAt;
+    this.layers.forEach(layer => layer.advance(timeElapsed));
+    this.lastAnimationFrameAt = timestamp;
+    window.requestAnimationFrame(this.animate);
+  }
+}
+
+export class Layer {
+  constructor (props) {
+    Object.assign(this, {
+      x: 0,
+      y: 0,
+      scale: 1,
+      rotation: 0,
+      rps: 180,
+      opacity: 1
+    }, props);
+
+    this.el = $('<div class="layer">').css({
+      opacity: this.opacity,
+      zIndex: this.zIndex
+    });
+
+    TweenMax.set(this.el, {
+      x: this.x,
+      y: this.y,
+      scale: this.scale,
+      rotation: this.rotation
+    })
+  }
+
+  remove () {
+    this.el.remove();
+  }
+
+  appendTo (el) {
+    this.el.appendTo(el);
+  }
+
+  advance (timeElapsed) {
+    this.rotation = (this.rotation + (this.rps * timeElapsed / 1000)) % 360;
+    TweenMax.set(this.el, { rotation: this.rotation });
+  }
+}
+
+export function Ease (exp) {
+  return {
+    in: (x) => Math.pow(x, exp),
+    out: (x) => 1 - Math.pow(Math.abs(x - 1), exp)
+  }
+}
+
+export function Range ({ from, to, scale, jitter }) {
+  return function (pos) {
+    const scaled = scale ? scale(pos) : pos;
+    let value = from + ((to - from) * scaled);
+    if (jitter) {
+      value = value + jitter - Math.random() * jitter * 2;
+    }
+    return value;
+  }
+}
+
+export class LayerGenerator {
+  constructor (ranges) {
+    this.ranges = ranges;
+  }
+
+  get (pos) {
+    const variant = {};
+    Object.keys(this.ranges).forEach((key) => { variant[key] = this.ranges[key](pos) });
+    return variant;
+  }
+
+  make (n, objects = []) {
+    for (let i = 0; i < n; i++) {
+      objects.push(this.get(i / n));
+    }
+    return objects;
+  }
+}
+
+$.fn.smoke = $.fn.smoke || function (method, ...args) {
+  let smoke = this.data('smoke');
+
+  if (!smoke) {
+    smoke = new Smoke(this);
+    this.data({ smoke });
+  }
+
+  if (!method) {
+    return this;
+  }
+
+  if (method === 'self') {
+    return smoke;
+  }
+
+  if (method === 'destroy') {
+    smoke.removeAllLayers();
+    this.data('smoke', null);
+    return this;
+  }
+
+  const target = Smoke.prototype[method];
+
+  if (typeof target !== 'function') {
+    throw new Error(`Unknown method '${method}' for smoke`);
+  }
+
+  return target.call(smoke, ...args);
+};
